46ad3ccf65b86e2743ad51f74a8fcec76d957a29
mhmttkmk <mehmet.tkmk.0352@gmail.com>
2019-05-06 03:36:33 +0300

    <z

--- myblade3.0/www/js/render.js
+++ myblade3.0/www/js/render.js
@@ -5,6 +5,10 @@ var ctx = myc.getContext("2d");
 myc.width = window.innerWidth;
 myc.height = window.innerHeight;
 
+var benimX = 0;
+var benimY = 0;
+var farkX = 0; //Benim X kordinatım ile ekranın tam ortasındaki X koordinatının farkı
+var farkY = 0; //Benim Y kordinatım ile ekranın tam ortasındaki Y koordinatının farkı
 
 
 var topac = new Image();
@@ -24,23 +28,14 @@ function zemin_Ciz(v){
 
 
 function topaci_ekranda_ortala(){
-	var ortaX = (myc.width/2)-(topac.width/2);
-	var ortaY = (myc.height/2)-(topac.height/2)
+	//var ortaX = Math.floor((myc.width/2)-(topac.width/2));
+	//var ortaY = Math.floor((myc.height/2)-(topac.height/2));
+	var ortaX = Math.floor((myc.width/2));
+	var ortaY = Math.floor((myc.height/2));	
 	return {ortaX: ortaX, ortaY: ortaY}
 }
 
-function topaci_Izle(x,y){
-	var ortaX = topaci_ekranda_ortala().ortaX;
-	var ortaY = topaci_ekranda_ortala().ortaY;
 
-	var farkX = ortaX-x;
-	var farkY = ortaY-y;
-
-	var x = x+farkX;
-	var y = y+farkY;
-	return {x: x, y: y}
-
-}
 
 function topac_Ciz(sid,sidBilgi){
 
@@ -55,26 +50,41 @@ function topac_Ciz(sid,sidBilgi){
 
 	var yaziRengi = "black";
 
+	var ortaX = topaci_ekranda_ortala().ortaX;
+	var ortaY = topaci_ekranda_ortala().ortaY;
+
 	if (sid && sid == s.id){
-		c("Benim bilgi");
 		yaziRengi = "red";
+
+		benimX =  sidBilgi.x;
+		benimY =  sidBilgi.y;
+
+		farkX = ortaX-benimX;
+		farkY = ortaY-benimY;
+
+
+
+
+		ctx.drawImage(topac, topac.width/7*sidBilgi.a, 0, topac.width/7, topac.height, ortaX, ortaY, topac.width/7, topac.height);		
+
 	}
 	else{
-		c("Diğerleri");
 		yaziRengi = "blue";
+		var x = sidBilgi.x+farkX;
+		var y = sidBilgi.y+farkY;
 
+		ctx.drawImage(topac, topac.width/7*sidBilgi.a, 0, topac.width/7, topac.height, x, y, topac.width/7, topac.height);	
 	}
 
 	ctx.clearRect(0, 0, topac.width, topac.height);
 	ctx.shadowColor = yaziRengi;
-	ctx.shadowOffsetX = 10;
-	ctx.shadowOffsetY = 15;
-	ctx.shadowBlur = 15;
+	ctx.shadowOffsetX = 5;
+	ctx.shadowOffsetY = 6;
+	ctx.shadowBlur = 6;
+
+	
 
 
-c(sidBilgi);
-
-	ctx.drawImage(topac, topac.width/7*sidBilgi.a, 0, topac.width/7, topac.height, sidBilgi.x, sidBilgi.y, topac.width/7, topac.height);
 }
 
 
